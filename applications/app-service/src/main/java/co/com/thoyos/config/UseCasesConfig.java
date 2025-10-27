9fd7e31bab8383dc1c87e22776a254da7bf40f60
TatianaHoyos <tatianavertel98@gmail.com>
2025-10-27 10:06:03 -0500

    se crea base del proyecto en arquitectura limpia, con el plugin de bancolombia

---
+++ applications/app-service/src/main/java/co/com/thoyos/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.thoyos.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.thoyos.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
---
+++ domain/model/src/main/java/co/com/thoyos/model/person/gateways/PersonRepository.java
@@ -0,0 +1,4 @@
+package co.com.thoyos.model.person.gateways;
+
+public interface PersonRepository {
+}
